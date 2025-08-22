b1774e69e9917f7518e7b731464b935fff31e24d
Lucas Segundo <lucas.fernandesdata@gmail.com>
2025-08-22 09:00:59 -0300

    test: add unit tests for createMessageService with mocked database interactions

---
+++ src/data/services/CreateMessage/index.spec.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect, vi } from 'vitest'
+import { createMessageService } from '.'
+import { mockDBMessage } from '@/infra/drizzle/mocks/Message'
+import { db } from '@/infra/drizzle'
+import { messages } from '@/infra/drizzle/schema'
+
+vi.mock('@/infra/drizzle', () => ({
+  db: {
+    insert: vi.fn(() => ({
+      values: vi.fn(() => ({
+        returning: vi.fn(),
+      })),
+    })),
+  },
+}))
+
+describe('createMessageService', () => {
+  it('should create a message', async () => {
+    const dbMessage = mockDBMessage()
+
+    vi.mocked(db.insert).mockReturnValue({
+      values: vi.fn().mockReturnValue({
+        returning: vi.fn().mockResolvedValue([dbMessage]),
+      }),
+    } as any)
+
+    const result = await createMessageService({
+      text: dbMessage.text,
+    })
+
+    expect(
+      db.insert(messages).values(dbMessage).returning,
+    ).toHaveBeenCalledWith({
+      id: messages.id,
+      text: messages.text,
+      likes: messages.likes,
+      createdAt: messages.createdAt,
+    })
+    expect(result).toEqual({
+      ...dbMessage,
+      id: dbMessage.id.toString(),
+    })
+  })
+})
