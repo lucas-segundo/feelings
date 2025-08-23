d3d579f3585e4810eea43c96f8a79fcde079645b
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-08-23 10:39:13 -0300

    feat: add User entity and mock data for user testing

---
+++ src/domain/entities/User/mock.ts
@@ -0,0 +1,8 @@
+import { faker } from '@faker-js/faker'
+import { User } from '.'
+
+export const mockUser: User = {
+  id: faker.string.uuid(),
+  name: faker.person.fullName(),
+  email: faker.internet.email(),
+}
