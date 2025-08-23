4ac1223b0896e66b4af6870482b81d82fb3112bb
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-08-23 09:50:34 -0300

    feat: enhance betterAuth integration with nextCookies plugin and createAuthClient export

--- src/infra/betterAuth/index.ts
+++ src/infra/betterAuth/index.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { db } from '../drizzle'
+import { nextCookies } from 'better-auth/next-js'
+import { createAuthClient } from 'better-auth/react'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -12,4 +14,7 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
+  plugins: [nextCookies()],
 })
+
+export const authClient = createAuthClient()
