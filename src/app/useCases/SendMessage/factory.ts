774ec20e9714be583518607084270852e3305f2d
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-09-03 09:45:36 -0300

    feat: integrate language detection into SendMessage use case and update related tests

--- src/app/useCases/SendMessage/factory.ts
+++ src/app/useCases/SendMessage/factory.ts
@@ -1,10 +1,12 @@
 import { makeSentimentAnalysisPort } from '@/app/ports/SentimentAnalysis/factory'
 import { SendMessageUseCase } from '.'
 import { makeCreateMessagePort } from '@/app/ports/CreateMessage/factory'
+import { makeDetectLanguagePort } from '@/app/ports/DetectLanguage/factory'
 
 export const makeSendMessageUseCase = (): SendMessageUseCase => {
   return new SendMessageUseCase(
     makeSentimentAnalysisPort(),
     makeCreateMessagePort(),
+    makeDetectLanguagePort(),
   )
 }
--- src/app/useCases/SendMessage/index.ts
+++ src/app/useCases/SendMessage/index.ts
@@ -1,6 +1,7 @@
 import { Message } from '@/app/entities/Message'
 import { SentimentNotPositive } from '@/app/errors/SentimentNotPositive'
 import { CreateMessagePort } from '@/app/ports/CreateMessage'
+import { DetectLanguagePort } from '@/app/ports/DetectLanguage'
 import { SentimentAnalysisPort } from '@/app/ports/SentimentAnalysis'
 
 export interface SendMessageUseCaseDTO {
@@ -12,15 +13,18 @@ export class SendMessageUseCase {
   constructor(
     private readonly sentimentAnalysisPort: SentimentAnalysisPort,
     private readonly createMessagePort: CreateMessagePort,
+    private readonly detectLanguagePort: DetectLanguagePort,
   ) {}
 
   async execute({ text, userID }: SendMessageUseCaseDTO): Promise<Message> {
     const analysis = await this.sentimentAnalysisPort.analyze(text)
 
     if (analysis.sentiment === 'positive') {
+      const { language } = await this.detectLanguagePort.detect(text)
       return await this.createMessagePort.create({
         text,
         userID,
+        language,
       })
     }
 
