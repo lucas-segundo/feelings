5dae416cb44766aca2db96c5867af5038017cde4
Lucas Segundo <lucas.fernandes.app@gmail.com>
2026-03-25 08:38:17 -0300

    chore: add console log for locale debugging in request configuration

--- src/presentation/i18n/request.ts
+++ src/presentation/i18n/request.ts
@@ -8,6 +8,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale
+  console.log('locale', locale)
 
   return {
     locale,
